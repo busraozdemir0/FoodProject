c7a99d9954e2dacfaa43baae4670dd421ebe3231
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-07-18 13:48:51 +0300

    Kategorilerin dropdownlist'e çekilmesi

--- FoodProject/Controllers/FoodController.cs
+++ FoodProject/Controllers/FoodController.cs
@@ -1,12 +1,16 @@
 ﻿using FoodProject.Data.Models;
 using FoodProject.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FoodProject.Controllers
 {
     public class FoodController : Controller
     {
         FoodRepository foodRepository = new FoodRepository();
+        Context context = new Context();
         public IActionResult Index()
         {
             return View(foodRepository.TList("Category")); // İlgili yiyeceğin kategori adını getirebilmek için
@@ -14,6 +18,13 @@ namespace FoodProject.Controllers
         [HttpGet]
         public IActionResult FoodAdd()
         {
+            List<SelectListItem> values = (from x in context.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text=x.CategoryName,
+                                               Value=x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.categories = values;
             return View();
         }
         [HttpPost]

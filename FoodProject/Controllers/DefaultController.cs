44ed47f1e78e642479be6a1b1c7956b93c7630ff
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-07-19 17:06:45 +0300

    Anasayfa'nın(ETicaret Teması) dahil edilmesi

---
+++ FoodProject/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodProject.Controllers
+{
+	[AllowAnonymous]
+	public class DefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ FoodProject/ViewComponents/CategoryGetList.cs
@@ -0,0 +1,15 @@
+﻿using FoodProject.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodProject.ViewComponents
+{
+	public class CategoryGetList:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			CategoryRepository categoryRepository = new CategoryRepository();
+			var categoryList = categoryRepository.TList();
+			return View(categoryList);
+		}
+	}
+}

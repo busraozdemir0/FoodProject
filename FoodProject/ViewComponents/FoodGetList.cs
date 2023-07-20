4f13647bbb62b34c6e8280e2b56f5d0dab8c6018
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-07-20 13:22:52 +0300

    anasayfada ürünlerin listelenmesi

---
+++ FoodProject/ViewComponents/FoodGetList.cs
@@ -0,0 +1,15 @@
+﻿using FoodProject.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodProject.ViewComponents
+{
+	public class FoodGetList:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			FoodRepository foodRepository = new FoodRepository();
+			var foodList = foodRepository.TList();
+			return View(foodList);
+		}
+	}
+}

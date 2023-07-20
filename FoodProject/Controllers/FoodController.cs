42aff96b1e0321990375b225368353b1b3f5f3c3
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-07-20 16:07:06 +0300

    Dosyadan fotoğraf seçerek veritabanına kaydetme (Food resim yükleme)

--- FoodProject/Controllers/FoodController.cs
+++ FoodProject/Controllers/FoodController.cs
@@ -1,8 +1,11 @@
-﻿using FoodProject.Data.Models;
+﻿using FoodProject.Data;
+using FoodProject.Data.Models;
 using FoodProject.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using X.PagedList;
 
@@ -12,9 +15,9 @@ namespace FoodProject.Controllers
     {
         FoodRepository foodRepository = new FoodRepository();
         Context context = new Context();
-        public IActionResult Index(int page=1)
+        public IActionResult Index(int page = 1)
         {
-            return View(foodRepository.TList("Category").ToPagedList(page,3)); // Sayfalama 1. sayfadan başlayıp her sayfada 3 veri olsun. // İlgili yiyeceğin kategori adını getirebilmek için
+            return View(foodRepository.TList("Category").ToPagedList(page, 3)); // Sayfalama 1. sayfadan başlayıp her sayfada 3 veri olsun. // İlgili yiyeceğin kategori adını getirebilmek için
         }
         [HttpGet]
         public IActionResult FoodAdd()
@@ -22,25 +25,36 @@ namespace FoodProject.Controllers
             List<SelectListItem> values = (from x in context.Categories.ToList()
                                            select new SelectListItem
                                            {
-                                               Text=x.CategoryName,
-                                               Value=x.CategoryID.ToString()
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
                                            }).ToList();
             ViewBag.categories = values;
             return View();
         }
         [HttpPost]
-        public IActionResult FoodAdd(Food food)
+        public IActionResult FoodAdd(FoodImage p)
         {
-            if (!ModelState.IsValid)
+            Food food = new Food();
+            if (p.ImageURL != null)
             {
-                return View("FoodAdd");
+                var extension = Path.GetExtension(p.ImageURL.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Resimler/", newImageName);
+                var stream = new FileStream(location, FileMode.Create);
+                p.ImageURL.CopyTo(stream);
+                food.ImageURL = newImageName;
             }
+            food.Name = p.Name;
+            food.Price = p.Price;
+            food.Stock = p.Stock;
+            food.Description = p.Description;
+            food.CategoryID = p.CategoryID;
             foodRepository.TAdd(food);
             return RedirectToAction("Index");
         }
         public IActionResult FoodDelete(int id)
         {
-            foodRepository.TDelete(new Food { FoodID=id});
+            foodRepository.TDelete(new Food { FoodID = id });
             return RedirectToAction("Index");
         }
         public IActionResult FoodGet(int id)
@@ -57,26 +71,37 @@ namespace FoodProject.Controllers
             var x = foodRepository.TGet(id);
             Food food = new Food()
             {
-                FoodID=x.FoodID,
-                CategoryID=x.CategoryID,
-                Name=x.Name,
-                Price=x.Price,
-                Stock=x.Stock,
-                Description=x.Description,
-                ImageURL=x.ImageURL
+                FoodID = x.FoodID,
+                CategoryID = x.CategoryID,
+                Name = x.Name,
+                Price = x.Price,
+                Stock = x.Stock,
+                Description = x.Description,
+                ImageURL = x.ImageURL
             };
             return View(food);
         }
         [HttpPost]
         public IActionResult FoodUpdate(Food food)
         {
-            var x=foodRepository.TGet(food.FoodID);
-            x.Name= food.Name;
-            x.Stock=food.Stock;
-            x.Price=food.Price;
-            x.Description=food.Description;
-            x.ImageURL=food.ImageURL;
-            x.CategoryID=food.CategoryID;
+            //FoodImage p = new FoodImage();
+            //if (p.ImageURL != null)
+            //{
+            //    var extension = Path.GetExtension(p.ImageURL.FileName);
+            //    var newImageName = Guid.NewGuid() + extension;
+            //    var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Resimler/", newImageName);
+            //    var stream = new FileStream(location, FileMode.Create);
+            //    p.ImageURL.CopyTo(stream);
+            //    food.ImageURL = newImageName;
+            //}
+
+            var x = foodRepository.TGet(food.FoodID);
+            x.Name = food.Name;
+            x.Stock = food.Stock;
+            x.Price = food.Price;
+            x.Description = food.Description;
+            x.ImageURL = food.ImageURL;
+            x.CategoryID = food.CategoryID;
             foodRepository.TUpdate(x); // x'e göre güncelleme yapacağız
             return RedirectToAction("Index");
         }
---
+++ FoodProject/Data/FoodImage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FoodProject.Data
+{
+    public class FoodImage
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public IFormFile ImageURL { get; set; }
+        public int Stock { get; set; }
+        public int CategoryID { get; set; }
+    }
+}

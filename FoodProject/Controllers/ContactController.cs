36be2836bf7d745c13bed057d6a35432cfc2bea1
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-07-28 15:51:51 +0300

    Admin tarafında contact sayfası ve contact details kısmı yapıldı

---
+++ FoodProject/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using FoodProject.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FoodProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var contact = context.Contacts.ToList();
+            return View(contact);
+        }
+        public IActionResult ContactDelete(int id)
+        {
+            var contactID = context.Contacts.Find(id);
+            context.Contacts.Remove(contactID);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult ContactDetails(int id)
+        {
+            var contactID = context.Contacts.Find(id);
+            return View(contactID);
+        }
+
+    }
+}

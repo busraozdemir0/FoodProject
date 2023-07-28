781b7e573ebde7da6141b1a100b435f72e66b7b6
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-07-28 15:20:43 +0300

    Admin subscribe sayfası yapıldı

---
+++ FoodProject/Controllers/SubscribeController.cs
@@ -0,0 +1,23 @@
+﻿using FoodProject.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FoodProject.Controllers
+{
+    public class SubscribeController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var subscribeMails=context.Subscribes.ToList();
+            return View(subscribeMails);
+        }
+        public IActionResult SubscribeDelete(int id)
+        {
+            var subscribeID = context.Subscribes.Find(id);
+            context.Subscribes.Remove(subscribeID);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

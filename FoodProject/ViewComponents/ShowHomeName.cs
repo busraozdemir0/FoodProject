038fb09c44c29cac28a4f4d5a511937802eb5f3f
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-08-08 17:38:51 +0300

    Tasarımsal düzenlemeler

---
+++ FoodProject/ViewComponents/ShowHomeName.cs
@@ -0,0 +1,15 @@
+﻿using FoodProject.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodProject.ViewComponents
+{
+    public class ShowHomeName:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var userName=User.Identity.Name;
+            ViewBag.username = userName;
+            return View();
+        }
+    }
+}
